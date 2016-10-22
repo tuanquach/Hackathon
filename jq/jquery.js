5104b19491f6ba6cba825064b4c2fb132036857b
John McLaughlin <jmclaughlin85@gmail.com>
2016-10-22 13:28:21 -0400

    Upload

---
+++ jq/jquery.js
@@ -0,0 +1,9 @@
+// Custom JQuery by John
+function(){
+  $(".navDropItem").on({
+      mouseenter: function(){
+          $(this).css("background-color", "blue");
+          $(this).css("opacity", "0.2");
+      }  
+  })   
+}
